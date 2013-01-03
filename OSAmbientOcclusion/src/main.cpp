f569a6d8934c689ef2e7d75975e210663628367b
studentls <studentls@molsim>
2013-01-03 13:01:51 +0100

    initial setup

---
+++ OSAmbientOcclusion/src/main.cpp
@@ -0,0 +1,14 @@
+// Object Space Ambient Occlusion Demo Project
+// (c) 2013 by L.Spiegelberg
+// This code may be redistributed or modified for any learning or teaching purposes
+// but not for any commercial uses
+// to gain further license informations please contact me via
+// spiegelb (at) in.tum.de
+
+#include "main.h"
+
+int main(int argc, char * argv[])
+{
+
+	return 0;
+}
---
+++ OSAmbientOcclusion/src/main.h
@@ -0,0 +1,21 @@
+// Object Space Ambient Occlusion Demo Project
+// (c) 2013 by L.Spiegelberg
+// This code may be redistributed or modified for any learning or teaching purposes
+// but not for any commercial uses
+// to gain further license informations please contact me via
+// spiegelb (at) in.tum.de
+
+#ifndef MAIN_HEADER_
+#define MAIN_HEADER_
+
+// needs boost & glfw installed to compile properly
+
+#include <iostream>
+#include <boost/thread.hpp>
+#include <gl/glfw.h>
+
+
+
+
+
+#endif
